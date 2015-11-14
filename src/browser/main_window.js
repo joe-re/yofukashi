06683fee2da58ccbed234726fc85b406a4d75915
joe-re <joe.tialtngo@gmail.com>
2015-11-15 01:41:25 +0900

    refactor structure

--- src/browser/main_window.js
+++ src/browser/main_window.js
@@ -3,7 +3,7 @@ import BrowserWindow from 'browser-window';
 export default class MainWindow {
   constructor() {
     this.mainWindow =  new BrowserWindow({ width: 800, height: 600 });
-    this.mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    this.mainWindow.loadUrl('file://' + __dirname + '/../renderer/index.html');
     this.mainWindow.on('closed', () => {
       this.mainWindow = null;
     });
