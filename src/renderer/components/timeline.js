a3a69475e6e575142fe74e67c3eca1be67632673
joe-re <joe.tialtngo@gmail.com>
2015-11-22 17:52:18 +0900

    separate tweet component from timeline

--- src/renderer/components/timeline.js
+++ src/renderer/components/timeline.js
@@ -1,34 +1,15 @@
+/* @flow */
 import React, { PropTypes } from 'react';
+import Tweet from './tweet';
 
 export default class Timeline extends React.Component {
-  render() {
+  render(): ReactElement {
     const { timeline, getTimeline } = this.props;
-    const textes = timeline.map((tweet) => {
-      return (
-        <div className="tweet" key={tweet.id}>
-          <div className="tweet-avator">
-            <img src={tweet.user.profile_image_url} height="48" width="48" />
-          </div>
-          <div className="tweet-body">
-            <div className="tweet-names">
-              <span className="tweet-display-name">
-                {tweet.user.name}
-              </span>
-              <span className="tweet-screen-name">
-                @{tweet.user.screen_name}
-              </span>
-            </div>
-            <div className="tweetText">
-              {tweet.text}
-            </div>
-          </div>
-        </div>
-      );
-    });
+    const tweets = timeline.map((tweet) => <Tweet key={tweet.id} tweet={tweet} />);
     return(
       <div>
         <button onClick={getTimeline}>GET!</button>
-        {textes}
+        {tweets}
       </div>
     );
   }
---
+++ src/renderer/components/tweet.js
@@ -0,0 +1,32 @@
+import React, { PropTypes } from 'react';
+
+export default class Tweet extends React.Component {
+  render() {
+    const { tweet } = this.props;
+
+    return (
+      <div className="tweet">
+        <div className="tweet-avator">
+          <img src={tweet.user.profile_image_url} height="48" width="48" />
+        </div>
+        <div className="tweet-body">
+          <div className="tweet-names">
+            <span className="tweet-display-name">
+              {tweet.user.name}
+            </span>
+            <span className="tweet-screen-name">
+              @{tweet.user.screen_name}
+            </span>
+            <div className="tweet-text">
+              {tweet.text}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Tweet.propTypes = {
+  tweet: PropTypes.object.isRequired
+};
